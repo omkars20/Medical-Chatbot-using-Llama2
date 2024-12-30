f269118ae787e99b7d2134793fc36f5229fd077d
omkar singh <singhomkar20.1995@gmail.com>
2024-12-30 17:23:55 +0530

    sync

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Omkar Singh',
+    author_email= 'singhomkar20.1995@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
